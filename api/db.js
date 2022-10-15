719068eb9127bd95b856ba46247cc397294aedfe
leaderbb <hoangphuonga3k31@gmail.com>
2022-10-15 11:26:57 +0700

    update db

--- api/db.js
+++ api/db.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "createcv"
+  host: process.env.DB_HOST || "sql6.freemysqlhosting.net",
+  user: process.env.DB_USER || "sql6526645",
+  password: process.env.DB_PASS || "4edbB5J7HP",
+  database: process.env.DB_NAME || "sql6526645"
 });
 
 module.exports = db
