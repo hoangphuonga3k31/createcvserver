6c73e32363ee9758c6b09571a8833c3cb584c673
leaderbb <hoangphuonga3k31@gmail.com>
2022-10-24 13:52:29 +0700

    update-db-hosting

--- api/db.js
+++ api/db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "sql6.freemysqlhosting.net",
-  user: process.env.DB_USER || "sql6526645",
-  password: process.env.DB_PASS || "4edbB5J7HP",
-  database: process.env.DB_NAME || "sql6526645"
+  user: process.env.DB_USER || "sql6528776",
+  password: process.env.DB_PASS || "HJFvSIgTs7",
+  database: process.env.DB_NAME || "sql6528776"
 });
 
 module.exports = db
